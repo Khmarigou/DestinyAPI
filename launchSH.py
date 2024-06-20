a824d498b72d17af041bbfabf66f7c8b7bac45b2
Khmarigou <luca.plc@outlook.fr>
2024-06-20 17:25:51 +0200

    Removing secret information

--- launchSH.py
+++ launchSH.py
@@ -4,12 +4,10 @@ from config import API_KEY, AUTH_URL, CLIENT_ID, CLIENT_SECRET, BASE_URL
 
 def getStrangeGearInventory(authRessources):
     memberShipId = authRessources.membership_id
-    characterId = get(authRessources, "/Platform/Destiny2/3/Profile/4611686018483525509/?components=100")['Response']['profile']['data']['characterIds'][0]
+    characterId = get(authRessources, f"/Platform/Destiny2/3/Profile/{memberShipId}/?components=100")['Response']['profile']['data']['characterIds'][0]
     return get(authRessources, f"/Platform/Destiny2/3/Profile/{memberShipId}/Character/{characterId}/Vendors/3751514131/?components=402,400,101,401")
 
 
 if __name__ == '__main__':
     authRessources = getAuthentified()
     strangeGearInventory = getStrangeGearInventory(authRessources)
-    
-    # charRessouces = get(authRessources, "/Platform/Destiny2/3/Profile/4611686018483525509/?components=200,201,205")
